0902a6b368329d468a86bfd078d82df5d298cc61
Abdelali Dousbah <abdelali.dousbah@outlook.com>
2023-08-17 20:51:58 +0100

    tweaking the script and add the function game()

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-let items = ["Rock", "Paper", "Scissors"];
-
-
+const items = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * items.length);
-    let compSelection = items[randomIndex];
-    return compSelection;
-};
-let computerSelection = getComputerChoice();
-let playerSelection = "Scissors";
+    return items[randomIndex];
+}
 
-function playRound(playerSelection , computerSelection){
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");}
-    else if (
-            (playerSelection === "Rock" && computerSelection === "Scissors") ||
-            (playerSelection === "Paper" && computerSelection === "Rock") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper")
-            )
-    {
-        console.log("you win! " + playerSelection + " beats " + computerSelection)
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + playerSelection + " doesn't beat " + computerSelection;
     }
-    else {
-        console.log("you lose! " + playerSelection + " doesnt beat " + computerSelection)
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors").trim();
+        
+        console.log("Player choice is " + playerSelection);
+        console.log("Computer choice is " + computerSelection);
+        
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
     }
-    
-    };
-    console.log("player choice is " + playerSelection);
-    console.log("Computer choice is " + computerSelection);
-    
-playRound(playerSelection,computerSelection);
+}
 
-
-
-
-
+// Call the game function to play 5 rounds
+game();
